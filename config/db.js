a4dc6659cd2ccb03d5ba24ea08527ef2014eb0a2
Salma <salma--95@hotmail.com>
2021-10-01 12:33:09 +0300

    change mongoose config

--- config/db.js
+++ config/db.js
@@ -5,9 +5,16 @@ require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
 const URI = process.env.MONGODB_URI
 
 async function connectDB(){
-    console.log(URI)
+    //console.log(URI)
     try {
-        await mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: true,
+            authSource: 'admin',
+            retryWrites: true,
+        }
+        await mongoose.connect(URI, options)
         console.log('Connected to '+URI)
     } catch (err) {
         console.error(err)
